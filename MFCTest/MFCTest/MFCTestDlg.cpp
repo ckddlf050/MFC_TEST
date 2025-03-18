d3bdb83e8ea50f62d90e8cde208665130b795d46
ckddlf050 <ckddlf050@naver.com>
2025-03-18 13:46:58 +0900

    MFC

---
+++ MFCTest/MFCTest/MFCTestDlg.cpp
@@ -0,0 +1,233 @@
+﻿
+// MFCTestDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCTest.h"
+#include "MFCTestDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCTestDlg 대화 상자
+
+
+
+CMFCTestDlg::CMFCTestDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCTEST_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCTestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT_INPUT, m_input);
+	DDX_Control(pDX, IDC_LIST_OUPUT, m_ouput);
+}
+
+BEGIN_MESSAGE_MAP(CMFCTestDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_ADD, &CMFCTestDlg::OnBnClickedButtonAdd)
+	ON_BN_CLICKED(IDC_BUTTON_DELETE, &CMFCTestDlg::OnBnClickedButtonDelete)
+	ON_BN_CLICKED(IDC_BUTTON_SAVE, &CMFCTestDlg::OnBnClickedButtonSave)
+	ON_BN_CLICKED(IDC_BUTTON_Load, &CMFCTestDlg::OnBnClickedButtonLoad)
+END_MESSAGE_MAP()
+
+
+// CMFCTestDlg 메시지 처리기
+
+BOOL CMFCTestDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCTestDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCTestDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCTestDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void CMFCTestDlg::OnBnClickedButtonAdd()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CString strText;
+	m_input.GetWindowText(strText);
+
+	if (!strText.IsEmpty()) {
+		m_ouput.AddString(strText);
+		m_input.SetWindowText(_T(""));
+	}
+	else {
+		MessageBox(_T("텍스트를 입력하세요!"), _T("알림"), MB_OK);
+	}
+}
+
+void CMFCTestDlg::OnBnClickedButtonDelete()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	int index = m_ouput.GetCurSel();
+	if (index != LB_ERR) {
+		m_ouput.DeleteString(index);
+	}
+	else {
+		MessageBox(_T("삭제할 항목을 선택하세요"), _T("알림"), MB_OK);
+	}
+}
+
+void CMFCTestDlg::OnBnClickedButtonSave()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CFileDialog dlg(FALSE, _T("txt"), _T("data.txt"), OFN_OVERWRITEPROMPT, _T("Text Files (*.txt)|*.txt||"));
+
+	if (dlg.DoModal() == IDOK) {
+		CString filePath = dlg.GetPathName();
+		CStdioFile file;
+
+		if (file.Open(filePath, CFile::modeCreate | CFile::modeWrite | CFile::typeText)) {
+			int count = m_ouput.GetCount();
+			for (int i = 0; i < count; i++) {
+				CString str;
+				m_ouput.GetText(i, str);
+				file.WriteString(str + _T("\n"));
+			}
+			file.Close();
+			MessageBox(_T("파일 저장 완료!"), _T("알림"), MB_OK);
+		}
+	}
+}
+
+void CMFCTestDlg::OnBnClickedButtonLoad()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CFileDialog dlg(TRUE, _T("txt"), _T("data.txt"), OFN_OVERWRITEPROMPT, _T("Text Files (*.txt)|*.txt||"));
+
+	if (dlg.DoModal() == IDOK) {
+		CString filePath = dlg.GetPathName();
+		CStdioFile file;
+
+		if (file.Open(filePath, CFile::modeRead  | CFile::typeText)) {
+			m_ouput.ResetContent();
+			CString strLine;
+
+			while (file.ReadString(strLine))
+			{
+				m_ouput.AddString(strLine);
+			}
+			
+			file.Close();
+			MessageBox(_T("파일 불러오기 완료!"), _T("알림"), MB_OK);
+		}
+	}
+}
